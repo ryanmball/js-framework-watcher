f3f3c608d0c4f19e97c087621a95ce02856fc598
ryan <ball.ryan.rb@gmail.com>
2021-08-04 12:44:11 -0600

    adds axios and fetch request for vuejs repo data - testing

--- src/views/Frameworks.vue
+++ src/views/Frameworks.vue
@@ -1,5 +1,32 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="home">
+    <h1>{{ heading }}</h1>
+    <p>
+      This app provides detail on which JS frameworks are most popular.
+      Popularity is determined based on the number of Stars, Watches and Forks
+      for each frameworks' GitHub repo.
+    </p>
   </div>
 </template>
+
+<style></style>
+
+<script>
+import axios from "axios";
+export default {
+  data: () => {
+    return {
+      heading: "JavaScript Framework Popularity",
+    };
+  },
+  created: () => {
+    axios.get("/repos/vuejs/vue").then((response) => {
+      console.log(response.data);
+    });
+    fetch("https://api.github.com/repos/vuejs/vue")
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  },
+  methods: {},
+};
+</script>
