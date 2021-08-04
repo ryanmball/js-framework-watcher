b735f05f7916fc89a4e3b61e6862276606cd3473
ryan <ball.ryan.rb@gmail.com>
2021-08-04 17:40:32 -0600

    adds axios request and basic data display for each framework

--- src/views/Frameworks.vue
+++ src/views/Frameworks.vue
@@ -6,6 +6,41 @@
       Popularity is determined based on the number of Stars, Watches and Forks
       for each frameworks' GitHub repo.
     </p>
+    <h3>{{ vueData.name }}</h3>
+    <p>
+      {{ vueData.description }} <br />
+      Watchers: {{ vueData.subscribers_count }} <br />
+      Stars: {{ vueData.watchers_count }} <br />
+      Forks: {{ vueData.forks }}
+    </p>
+    <h3>{{ angularData.name }}</h3>
+    <p>
+      {{ angularData.description }} <br />
+      Watchers: {{ angularData.subscribers_count }} <br />
+      Stars: {{ angularData.watchers_count }} <br />
+      Forks: {{ angularData.forks }}
+    </p>
+    <h3>{{ emberData.name }}</h3>
+    <p>
+      {{ emberData.description }} <br />
+      Watchers: {{ emberData.subscribers_count }} <br />
+      Stars: {{ emberData.watchers_count }} <br />
+      Forks: {{ emberData.forks }}
+    </p>
+    <h3>{{ svelteData.name }}</h3>
+    <p>
+      {{ svelteData.description }} <br />
+      Watchers: {{ svelteData.subscribers_count }} <br />
+      Stars: {{ svelteData.watchers_count }} <br />
+      Forks: {{ svelteData.forks }}
+    </p>
+    <h3>{{ reactData.name }}</h3>
+    <p>
+      {{ reactData.description }} <br />
+      Watchers: {{ reactData.subscribers_count }} <br />
+      Stars: {{ reactData.watchers_count }} <br />
+      Forks: {{ reactData.forks }}
+    </p>
   </div>
 </template>
 
@@ -14,18 +49,43 @@
 <script>
 import axios from "axios";
 export default {
-  data: () => {
+  data: function () {
     return {
       heading: "JavaScript Framework Popularity",
+      vueData: {},
+      angularData: {},
+      emberData: {},
+      svelteData: {},
+      reactData: {},
     };
   },
-  created: () => {
+  created: function () {
     axios.get("/repos/vuejs/vue").then((response) => {
       console.log(response.data);
+      this.vueData = response.data;
     });
-    fetch("https://api.github.com/repos/vuejs/vue")
-      .then((response) => response.json())
-      .then((data) => console.log(data));
+    axios.get("/repos/angular/angular.js").then((response) => {
+      console.log(response.data);
+      this.angularData = response.data;
+    });
+    axios.get("/repos/emberjs/ember.js").then((response) => {
+      console.log(response.data);
+      this.emberData = response.data;
+    });
+    axios.get("/repos/sveltejs/svelte").then((response) => {
+      console.log(response.data);
+      this.svelteData = response.data;
+    });
+    axios.get("/repos/facebook/react").then((response) => {
+      console.log(response.data);
+      this.reactData = response.data;
+    });
+    // fetch("https://api.github.com/repos/vuejs/vue")
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     console.log(data);
+    //     this.vueData = data;
+    //   });
   },
   methods: {},
 };
