35bdfcf47c20d1b7628f710478d1be8113d6c7ec
ryan <ball.ryan.rb@gmail.com>
2021-08-09 19:25:06 -0600

    adds vue-fusioncharts

--- src/main.js
+++ src/main.js
@@ -2,11 +2,17 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import axios from "axios";
+import VueFusionCharts from "vue-fusioncharts";
+import FusionCharts from "fusioncharts";
+import Column2D from "fusioncharts/fusioncharts.charts";
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
 
 axios.defaults.baseURL =
   process.env.NODE_ENV === "development" ? "https://api.github.com" : "/";
 
 Vue.config.productionTip = false;
+
+Vue.use(VueFusionCharts, FusionCharts, Column2D, FusionTheme);
 
 new Vue({
   router,
